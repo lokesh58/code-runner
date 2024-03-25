2ea1e0b2a54248dfb080bc431d20536336393d6a
Lokesh Patel <32188450+lokesh58@users.noreply.github.com>
2024-03-25 16:46:51 +0530

    complete implementation for get_all_languages util

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,11 +1,40 @@
+use phf::{phf_map, Map};
+
 use crate::models::{LanguageDetails, RunCodeParams, RunCodeResult};
 
+struct LanguageInfo {
+    display: &'static str,
+}
+
+static LANGUAGE_INFO_MAP: Map<&'static str, LanguageInfo> = phf_map! {
+    "c" => LanguageInfo {
+        display: "C",
+    },
+    "cpp" => LanguageInfo {
+        display: "C++",
+    },
+    "js" => LanguageInfo {
+        display: "JavaScript",
+    },
+    "ts" => LanguageInfo {
+        display: "TypeScript",
+    },
+    "py" => LanguageInfo {
+        display: "Python",
+    },
+    "rs" => LanguageInfo {
+        display: "Rust",
+    },
+};
+
 pub fn get_all_languages() -> Vec<LanguageDetails> {
-    // TODO: Implement this
-    vec![LanguageDetails {
-        language: String::from("dummy"),
-        display: String::from("Dummy Language"),
-    }]
+    LANGUAGE_INFO_MAP
+        .entries()
+        .map(|(lang, info)| LanguageDetails {
+            language: lang.to_string(),
+            display: info.display.to_string(),
+        })
+        .collect()
 }
 
 pub async fn run_code(params: RunCodeParams) -> RunCodeResult {
