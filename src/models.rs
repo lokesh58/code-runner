44b5c5d011843da71a53bbdd543c68b6fa0ee3ad
Lokesh Patel <32188450+lokesh58@users.noreply.github.com>
2024-03-25 13:28:49 +0530

    add api response handling

--- src/models.rs
+++ src/models.rs
@@ -1,8 +1,20 @@
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize)]
+pub struct LanguageDetails {
+    pub language: String,
+    pub display: String,
+}
 
 #[derive(Deserialize)]
 pub struct RunCodeParams {
     pub language: String,
     pub code: String,
     pub input: Option<String>,
+}
+
+#[derive(Serialize)]
+pub struct RunCodeResult {
+    pub output: String,
+    pub error: String,
 }
--- src/routes.rs
+++ src/routes.rs
@@ -1,20 +1,26 @@
-use actix_web::{get, post, web, Error, HttpResponse};
-use code_runner::models::RunCodeParams;
+use actix_web::{get, post, web, Responder, Result};
+use code_runner::models::{LanguageDetails, RunCodeParams, RunCodeResult};
 
 #[get("/list")]
-async fn list_supported_languages() -> Result<HttpResponse, Error> {
-    Ok(HttpResponse::Ok().body("TODO: return list"))
+async fn list_supported_languages() -> Result<impl Responder> {
+    Ok(web::Json(vec![LanguageDetails {
+        language: String::from("dummy"),
+        display: String::from("Dummy Lang for test"),
+    }]))
 }
 
 #[post("/run")]
-async fn run_code(params: web::Json<RunCodeParams>) -> Result<HttpResponse, Error> {
+async fn run_code(params: web::Json<RunCodeParams>) -> Result<impl Responder> {
     println!(
         "Received params:\nlanguage: {},\ncode: {},\ninput: {}",
         params.language,
         params.code,
         params.input.clone().unwrap_or(String::from("<empty>"))
     );
-    Ok(HttpResponse::Ok().body("TODO: Return code output"))
+    Ok(web::Json(RunCodeResult {
+        output: String::from("TODO: Run code to get actual output"),
+        error: String::from("TODO: Run code to get actual error"),
+    }))
 }
 
 pub fn setup_routes(cfg: &mut web::ServiceConfig) {
